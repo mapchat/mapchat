6dba628a6d87f3eb74df1da5371212bbe22f8077
Fedor Indutny <fedor.indutny@gmail.com>
2011-01-30 20:21:52 +0600

    [install.sh] corrected install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
-
+clear
 echo "Installing MapChat on CouchOne instance" >&2
 echo "" >&2
-
-git=`which git 2>&1`
-ret=$?
-if [ $ret -ne 0 ] || ! [ -x $git ]; then
-  echo "MapChat cannot be installed without git." >&2
-  echo "Install git first, and then try again." >&2
-  exit $ret
-fi
 
 read -p "Instance name (instname.couchone.com): " INSTANCE
 read -p "Database name: " DB
@@ -23,25 +15,7 @@ echo "" >&2
 echo "Starting installation" >&2
 echo "" >&2
 
-TMP="${TMPDIR}"
-if [ "x$TMP" = "x" ]; then
-  TMP="/tmp"
-fi
-TMP="${TMP}/mapchat.$$"
-rm -rf "$TMP" || true
-mkdir "$TMP"
-if [ $? -ne 0 ]; then
-  echo "failed to mkdir $TMP" >&2
-  exit 1
-fi
-
-echo "Cloning git repo..." >&2
-echo "" >&2
-cd "$TMP" \
-&& git clone git://github.com/mapchat/mapchat.git \
-&& cd mapchat \
-&& echo "" >&2 \
-&& echo "Pushing couchapp..." >&2 \
+echo "Pushing couchapp..." >&2 \
 && echo "" >&2 \
 && couchapp push . http://$USER:$PASSWORD@$INSTANCE.couchone.com/$DB \
 && cd _auth \
@@ -54,3 +28,5 @@ echo "" >&2
 echo "Installation finished" >&2
 echo "Visit http://$INSTANCE.couchone.com/$DB/_design/mapchat/_rewrite/ and start conversation on a MapChat!" >&2
 echo "" >&2
+exit 0
+
