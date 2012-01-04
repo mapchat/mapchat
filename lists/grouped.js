bc4062cf782db9ba773a1aac38848f07e924a897
Fedor Indutny <fedor.indutny@gmail.com>
2012-01-04 18:51:50 +0600

    [spatial index] added lowest date limit (2w)

--- lists/grouped.js
+++ lists/grouped.js
@@ -8,9 +8,12 @@ function(head, req) {
   send('[');
 
   var last = [-1e3, -1e3],
+      lowestDate = +new Date - 14 * 24 * 3600 * 1000,
       first = true;
 
   while (row = getRow()) {
+    if (row.value.created_at < lowestDate) continue;
+
     var current = row.value.loc;
     if (current[0] == last[0] && current[1] == last[1]) continue;
 
